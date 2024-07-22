7f7d78432abeb596771d2e11dd8d65ceaa3a0552
AnujSinghR <anujsingh0r@gmail.com>
2024-07-22 10:42:56 +0530

    some change

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/",(req,res) => res.send("Express on vercel"));
+app.get("/",(req,res) => res.send("Anuj singh"));
 app.listen(3000, ()=>console.log("server ready in port 3000"));
 
 module.exports = app;
