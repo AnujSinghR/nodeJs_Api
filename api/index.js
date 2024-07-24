3b0cac7d731f3a95fabac6fde19ccd4d25bc242f
AnujSinghR <anujsingh0r@gmail.com>
2024-07-24 10:40:34 +0530

    implement login api in this api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res) => res.send("Anuj singh"));
+app.get("/login",(req,res) => res.send("login Works"));
 app.listen(3000, ()=>console.log("server ready in port 3000"));
+
 
 module.exports = app;
