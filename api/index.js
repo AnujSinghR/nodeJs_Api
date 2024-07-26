b14d1497834b2aa3040b00e7736ad8339ccd4769
AnujSinghR <anujsingh0r@gmail.com>
2024-07-26 10:09:36 +0530

    some changes like adding env file of mongo uri

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 // mongo db crud operations
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const uri = "mongodb+srv://vercel-admin-user:JSdjiSgjTzMS3cUy@cluster6.mfwkoxk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
+const uri = process.env.MONGODB_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
