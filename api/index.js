bbd8439bc89ee6306e0d6de13adc44637ec2f422
AnujSinghR <anujsingh0r@gmail.com>
2024-07-29 10:48:10 +0530

    some changes

--- api/index.js
+++ api/index.js
@@ -1,53 +1,55 @@
-// mongo db crud operations
 require('dotenv').config();
-//const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const PORT = 3000;
 const uri = process.env.MONGODB_URI;
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    console.log("Trying to connect with server");
-    await mongoose.connect(uri).then(()=>console.log("mongo db connected"));
-    console.log("connected");
-    const userSchema = new mongoose.Schema({
-      firstName:{
-          type:String,
-          required:true
-      },
-      email:{
-          type:String,
-          required:true,
-      }
-  })
-  const user = mongoose.model("Login_cred",userSchema)
-  await user.create({
-      firstName:"it finally works",
-      email:"newuserbyvercel@gmail.com"
-  })
-  }catch(error){
-    console.log("Err",error); 
+app.use(express.json());
 
- } finally {
-    // Ensures that the client will close when you finish/error
-  }
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+    }
+});
+
+const User = mongoose.model("Login_cred", userSchema);
+
+async function connectToDatabase() {
+    try {
+        console.log("Trying to connect to the server");
+        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("Connection error:", error);
+        throw error;
+    }
 }
 
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
     res.send('Hello, World!');
-  });
+});
 
-// test db connection
-app.get('/anuj',(req,res)=>{
-    run();
-    res.send(process.env.MONGODB_URI);
-})
+// Test DB connection and fetch all users
+app.get('/anuj', async (req, res) => {
+    try {
+        await connectToDatabase();
+        const users = await User.find({});
+        res.json(users);
+    } catch (error) {
+        res.status(500).send("Error connecting to the database or fetching users");
+    } finally {
+        mongoose.connection.close();
+    }
+});
 
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-  });
+});
